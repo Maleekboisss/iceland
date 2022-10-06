b968697b5cb1ebba88d74312afe9206592e1689f
Maleek Boisseau <maleekboisseau@Maleeks-MacBook-Pro.local>
2022-10-05 18:28:07 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Glassweb from '../src/Pages/Glassweb'
+
+
+function App() {
+  return (
+    <>
+    <Glassweb />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Glassweb.js
@@ -0,0 +1,741 @@
+import React from 'react'
+import styled from 'styled-components'
+import { BsFacebook } from 'react-icons/bs';
+import { SiTwitter } from 'react-icons/si';
+import { BsInstagram } from 'react-icons/bs';
+import { ImTumblr } from 'react-icons/im';
+import { FaLinkedin } from 'react-icons/fa';
+import { SiNike } from 'react-icons/si';
+import { SiAdidas } from 'react-icons/si';
+import { BsApple } from 'react-icons/bs';
+import { GrBitcoin } from 'react-icons/gr';
+import { RiVisaLine} from 'react-icons/ri';
+
+
+
+
+
+function Glassweb() {
+  return (
+    <>
+    <Page>
+    <header>
+    <nav>
+      <div class="container">
+        <div class="menu">
+		  <ul class="social-media">
+            <li><a href="#" class="media-icon"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="#" class="media-icon"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#" class="media-icon"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="#" class="media-icon"><i class="fab fa-pinterest"></i></a></li>
+            <li><a href="#" class="media-icon"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="#" class="media-icon"><i class="fab fa-youtube"></i></a></li>
+          </ul>
+          <ul class="nav-links">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </ul>
+        </div>
+        <div class="menu-btn">
+          <i class="fas fa-bars"></i>
+        </div>
+      </div>
+    </nav>
+    <div class="glass">
+	  <h1 class="h-glass">Ice</h1>
+	  <div>
+	    <div>
+		  <i class="fas fa-route fa-2x"></i>
+		</div>
+		<div>
+		  <h2>Discover Tours</h2>
+		  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel hendrerit quam. Sed tempus dolor ac dictum euismod.</p>
+		  <a href="#">View
+		    <i class="fas fa-long-arrow-alt-right"></i>
+		  </a>
+		</div>
+	  </div>
+	</div>
+	 <h1 class="h-regular">Land</h1>
+  </header>
+  
+  <section>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel hendrerit quam. Sed tempus dolor ac dictum euismod. Aenean nec nibh a augue consectetur congue. Phasellus vitae placerat nibh. Vestibulum interdum aliquam magna, vitae tempor nibh tempus in. Etiam ut felis molestie, faucibus quam eget, feugiat purus. In diam lacus, imperdiet et risus in, tempus sollicitudin purus.</p>
+    <p>Mauris suscipit consectetur euismod. Suspendisse a pellentesque justo. In nulla odio, varius ut dolor in, consequat auctor urna. Etiam malesuada aliquam tortor nec bibendum. Suspendisse maximus nisl eu nunc sagittis faucibus vitae at velit. Aliquam posuere, justo nec feugiat ultricies, nunc massa placerat metus, eu tincidunt orci libero sit amet nisi. Aliquam eget massa commodo, sollicitudin nibh in, vestibulum ante. Donec at mauris turpis. Proin porta eros eu nibh faucibus pretium. In hac habitasse platea dictumst. Nunc in nunc metus. Mauris bibendum mollis metus, sed fringilla augue.</p>
+    <p>Praesent laoreet aliquet purus, ut molestie metus pulvinar eu. Quisque at neque sed dolor gravida maximus sed sit amet lectus. Proin sapien enim, porttitor sed libero nec, pulvinar sagittis ex. Etiam finibus libero nisi, ac elementum sapien vestibulum eget. Duis scelerisque mi eu diam malesuada, ac dapibus est efficitur. Phasellus eget lorem eget massa aliquam maximus. Curabitur sit amet massa iaculis, venenatis nisi ac, vestibulum lectus. Sed malesuada, nisl eu porttitor consectetur, risus leo commodo nunc, tincidunt pharetra justo ipsum eget augue. Nunc sagittis tortor bibendum libero molestie, ultrices dapibus justo fermentum. Curabitur dignissim a sapien quis efficitur. Nunc ex nibh, malesuada id leo ut, ultrices gravida sem. Fusce dictum, ante maximus imperdiet ullamcorper, libero libero auctor urna, vitae ultricies elit risus at ipsum. Sed quis lobortis nibh. Praesent viverra convallis convallis. Quisque rhoncus diam sit amet congue malesuada.</p>
+  </section>
+
+  <br />
+
+  <section>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel hendrerit quam. Sed tempus dolor ac dictum euismod. Aenean nec nibh a augue consectetur congue. Phasellus vitae placerat nibh. Vestibulum interdum aliquam magna, vitae tempor nibh tempus in. Etiam ut felis molestie, faucibus quam eget, feugiat purus. In diam lacus, imperdiet et risus in, tempus sollicitudin purus.</p>
+    <p>Mauris suscipit consectetur euismod. Suspendisse a pellentesque justo. In nulla odio, varius ut dolor in, consequat auctor urna. Etiam malesuada aliquam tortor nec bibendum. Suspendisse maximus nisl eu nunc sagittis faucibus vitae at velit. Aliquam posuere, justo nec feugiat ultricies, nunc massa placerat metus, eu tincidunt orci libero sit amet nisi. Aliquam eget massa commodo, sollicitudin nibh in, vestibulum ante. Donec at mauris turpis. Proin porta eros eu nibh faucibus pretium. In hac habitasse platea dictumst. Nunc in nunc metus. Mauris bibendum mollis metus, sed fringilla augue.</p>
+    <p>Praesent laoreet aliquet purus, ut molestie metus pulvinar eu. Quisque at neque sed dolor gravida maximus sed sit amet lectus. Proin sapien enim, porttitor sed libero nec, pulvinar sagittis ex. Etiam finibus libero nisi, ac elementum sapien vestibulum eget. Duis scelerisque mi eu diam malesuada, ac dapibus est efficitur. Phasellus eget lorem eget massa aliquam maximus. Curabitur sit amet massa iaculis, venenatis nisi ac, vestibulum lectus. Sed malesuada, nisl eu porttitor consectetur, risus leo commodo nunc, tincidunt pharetra justo ipsum eget augue. Nunc sagittis tortor bibendum libero molestie, ultrices dapibus justo fermentum. Curabitur dignissim a sapien quis efficitur. Nunc ex nibh, malesuada id leo ut, ultrices gravida sem. Fusce dictum, ante maximus imperdiet ullamcorper, libero libero auctor urna, vitae ultricies elit risus at ipsum. Sed quis lobortis nibh. Praesent viverra convallis convallis. Quisque rhoncus diam sit amet congue malesuada.</p>
+  </section>
+
+  <Footer>
+  <footer class="footer1">
+      <div class="footer-top">
+        <div class="container">
+          <div class="col1">
+            <a href="#" class="brand">Brand</a>
+            <ul class="media-icons">
+              <li>
+                <a href="#">
+                  <BsFacebook />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <SiTwitter />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <BsInstagram />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <ImTumblr />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaLinkedin />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col2">
+            <ul class="menu">
+              <li>
+                <h4>About</h4>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Pricing</a>
+              </li>
+              <li>
+                <a href="#">Customers</a>
+              </li>
+              <li>
+                <a href="#">Carrers</a>
+              </li>
+            </ul>
+            <ul class="menu">
+              <li>
+                <h4>Resources</h4>
+              </li>
+              <li>
+                <a href="#">Docs</a>
+              </li>
+              <li>
+                <a href="#">Block</a>
+              </li>
+              <li>
+                <a href="#">eBooks</a>
+              </li>
+              <li>
+                <a href="#">Webinars</a>
+              </li>
+            </ul>
+            <ul class="menu">
+              <li>
+                <h4>Contact</h4>
+              </li>
+              <li>
+                <a href="#">Help</a>
+              </li>
+              <li>
+                <a href="#">Sales</a>
+              </li>
+              <li>
+                <a href="#">Advertise</a>
+              </li>
+              <li>
+                <a href="#">Location</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col3">
+            <h4>Additional Info</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis urna massa, eget mattis ligula consequat eu. Nam quis arcu non risus consequat blandit sed at mauris.
+            </p>
+          </div>
+        </div>
+        <div class="footer-bottom">
+          <div class="container">
+            <p>
+                Quis irure ipsum adipisicing incididunt.
+            </p>
+            <ul class="service-icons">
+              <li>
+                <a href="#">
+                  <SiNike />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <SiAdidas />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <BsApple />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <GrBitcoin />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <RiVisaLine />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>   
+    </Footer>
+
+    </Page>
+    </>
+  )
+}
+
+export default Glassweb
+
+const Page = styled.div`
+
+nav {
+    z-index: 99;
+    position: relative;
+    transition: 0.4s;
+  }
+  
+  nav a {
+    text-decoration: none;
+    color: #1f1f1f;
+  }
+  
+  nav .container {
+    max-width: 1150px;
+    padding: 1em 2em;
+    margin: 0 auto;
+  }
+  
+  nav .menu {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  nav ul {
+    list-style-type: none;
+    padding: 0;
+    display: inline-block;
+  }
+  
+  nav ul li{
+    display: inline-block;
+    margin: 0 0.5em;
+  }
+  
+  nav ul a {
+    font-size: 0.8em;
+    transition: 0.5s ease;
+    text-transform: uppercase;
+  }
+
+  nav ul a:hover {
+    box-shadow: 0px 0px 10px black;
+    background-color: white;
+    letter-spacing: 0.3rem;
+    border-radius: 1rem;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    color: red;
+  }
+
+  
+  nav .container .menu-btn {
+    display: none;
+  }
+  
+  .social-media a {
+    font-size: 1em;
+  }
+  
+  header {
+    height: 100vh;
+    background: 
+      url(https://source.unsplash.com/rPkC3viHxug/1600x900) 
+      center no-repeat fixed;
+    background-size: cover;
+    position: relative;
+    overflow: hidden;
+  }
+  
+  header h1 {
+    position: absolute;
+    font-size: clamp(3.5rem, 15vw + 1rem, 12rem);  
+    text-transform: lowercase;
+    letter-spacing: 5px;
+    font-weight: 700;
+    transform: translateY(-50%);
+    opacity: 0;
+  }
+  
+  .glass {
+    position: absolute;
+    width: 42%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.3);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    border: 1px solid rgba(255, 255, 255, 0.18);
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+  
+  .h-glass {
+    background: 
+      url(https://source.unsplash.com/rPkC3viHxug/1600x900) 
+      center no-repeat fixed;
+    background-size: cover;
+    color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text; 
+    -webkit-text-fill-color: transparent;
+    top: 20%;
+    right: 0;
+    animation: text1 0.8s ease-in forwards;
+  }
+  
+  .h-regular {
+    color: #fff;
+    top: 40%;
+    left: 42%;
+    animation: text2 0.8s ease-in forwards;
+    animation-delay: 500ms;
+  }
+  
+  .glass > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 1.5em;
+  }
+  
+  .glass > div div {
+    margin: 0 1em;
+  }
+  
+  .glass p {
+    color: #ccc;
+    font-size: 0.9em;
+    line-height: 27px;
+    max-width: 27em;
+  }
+  
+  .glass h2 {
+    color: #fff;
+  }
+  
+  .glass a {
+   color: #fff;
+   text-decoration: none;
+  }
+  
+  .glass a i {
+    margin-left: 0.2em;
+  }
+  
+  .glass .fa-route {
+    color: #ccc;
+  }
+  
+  section {
+    padding: 2em;
+    background: #f1f1f1;
+    color: #1f1f1f;
+    font-size: 1.1em;
+    line-height: 32px;
+  }
+  
+  section p {
+    max-width: 900px;
+  }
+      
+  @media screen and (max-width: 900px) {
+    nav .container {
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-start;
+    }
+    nav .menu {
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      height: 0;
+      overflow: hidden;
+      transition: 0.7s;
+    }
+    .nav-links li {
+      display: block;
+      margin: 0.5em 0;
+    }
+    .social-media li:nth-child(1) {
+      margin-left: 0;
+    }
+    .social-media  {
+      order: 2;
+    }
+    .nav-links {
+      order: 1;
+      padding-top: 1em;
+    }
+    nav .container .menu-btn {
+      display: inline-block;
+      font-size: 1.4em;
+      cursor: pointer;
+    }
+    nav .container .nav-toggle {
+      height: 15em;
+    }
+    .nav-background {
+      background: #fff;
+    }
+  }
+  
+  @media screen and (max-width: 750px) {
+    .glass {
+      width: 90%;
+      height: initial;
+      min-height: 30em;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 15px;
+    }
+    .h-glass {
+      right: 52%;
+      top: 10%;
+      color: #1f1f1f;
+      background-attachment: initial;
+      background-size: 100% 130%;
+    }
+    .h-regular {
+      top: 30%;
+    }
+    nav .container .menu-btn {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    .glass > div {
+      margin-top: 4em;
+    }
+  }
+  
+  @media screen and (max-height: 700px) {
+    header {
+      height: 700px;
+    }
+  }
+  
+  
+  @media screen and (max-width: 500px) {
+    .glass > div div:nth-child(1) {
+      display: none;
+    }
+  }
+  
+  @keyframes text1 {
+    0% {
+      opacity: 0;
+      transform: translate(-2em, -50%);
+    }
+    100% {
+      opacity: 1;
+      transform: translate(0, -50%);
+    }
+  }
+  
+  @keyframes text2 {
+    0% {
+      opacity: 0;
+      transform: translate(2em, -50%);
+    }
+    100% {
+      opacity: 1;
+      transform: translate(0, -50%);
+    }
+  }
+`
+
+const Footer = styled.div`
+
+h1 {
+    color: #fff;
+    text-align: center;
+    margin: 3em 0;
+    font-size: 3em;
+  }
+  
+  /*Footer styles start here*/
+  footer * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  
+  footer ul {
+    list-style: none;
+  }
+  
+  footer {
+    color: #fff;
+    background: #121315;
+  }
+  
+  footer p {
+    color: #ccc; 
+    max-width: 25em;
+    font-size: 0.9em;
+    line-height: 23px;
+  }
+  
+  footer a {
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  footer h4 {
+    margin-bottom: 1em;
+    text-transform: uppercase;
+  }
+  
+  footer .container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 2em;
+  }
+  
+  .footer1 {
+    position: relative;
+    /*BG image in description*/
+    background: url(https://source.unsplash.com/VviFtDJakYk/1600x900) no-repeat center;
+    background-size: cover;
+  }
+  
+  .footer1::before {
+    content: '';
+    background: rgba(0, 0, 0, 0.8);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+  }
+  
+  .footer1 .container {
+    position: relative;
+  }
+  
+  .footer1 .footer-top .container,
+  .footer1 .col2,
+  .footer1 .footer-bottom .container,
+  .footer2 .container,
+  .footer3 .container {
+    display: flex;
+    justify-content: space-between;
+    gap: 2em;
+  }
+  
+  .footer1 .footer-bottom .container {
+    padding: 1em 2em;
+    align-items: center;
+  }
+  
+  .footer1 .menu li {
+    margin-bottom: 0.6em;
+  }
+  
+  .footer1 .menu a:hover {
+    text-decoration: underline;
+  }
+  
+  .brand {
+    display: block;
+    font-size: 1.8em;
+    font-weight: 600;
+    margin-bottom: 1em;
+  }
+  
+  .media-icons li,
+  .service-icons li {
+    display: inline-block;
+    margin: 0.5em 0.5em 0.5em 0;
+  }
+  
+  .media-icons a {
+    font-size: 1.1em;
+    width: 2em;
+    height: 2em;
+    border: 1px #fff solid;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: 0.3s;
+  }
+  
+  .media-icons a:hover {
+    background: #fff;
+    color: red;
+  }
+  
+  .service-icons a {
+    font-size: 1.6em;
+    margin-right: 0.3rem;
+  }
+  
+  .service-icons a:hover {
+    color: red;
+  }
+  
+  .footer-bottom {
+    border-top: 1px #dbdbdb solid;
+  }
+  
+  .footer2 .menu {
+    margin-bottom: 3em;
+  }
+  
+  .footer2 .menu li,
+  .footer4 .menu li {
+    display: inline-block;
+    margin: 0.7em 0.7em 0.7em 0;
+    text-align: center; 
+  }
+  
+  .footer2 .menu a,
+  .footer4 .menu a  {
+    transition: 0.3s;
+    font-weight: 600;
+  }
+  
+  .footer2 .menu a:hover,
+  .footer4 .menu a:hover {
+    color: #ff2b00;
+  }
+  
+  .input-wrap {
+    margin: 1em 0;
+    display: flex;
+  }
+  
+  .input-wrap input {
+    padding: 0.5em;
+    border: none;
+    background: #20232a;
+    color: #ccc;
+  }
+  
+  .input-wrap input:focus {
+    outline: none;
+  }
+  
+  .input-wrap button {
+    padding: 0.8em;
+    background: #ff2b00;
+    border: none;
+    color: #fff;
+  }
+  
+  .input-wrap button:hover {
+    background: #fff;
+    color: #111;
+  }
+  
+  .footer3 .container {
+    align-items: center;
+    text-align: center;
+  }
+  
+  .footer3 .brand {
+    margin: 0;
+  }
+  
+  .footer3 .menu li {
+    display: inline-block;
+    margin: 1em;
+    text-align: center;
+  }
+  
+  .footer3 .menu a  {
+    transition: 0.3s;
+    font-weight: 300;
+    color: #ccc;
+  }
+  
+  .footer3 .menu a:hover {
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  .footer4 {
+    text-align: center;
+  }
+  
+  .footer4 p {
+    margin: 0 auto;
+  }
+  
+  .footer4 .media-icons {
+    margin: 1.5em 0;
+  }
+  
+  @media screen and (max-width: 900px) {
+    .footer1 .footer-top .container,
+    .footer1 .footer-bottom .container,
+    .footer2 .container,
+    .footer3 .container  {
+      flex-direction: column;
+    }
+    .footer1 .col2  {
+      justify-content: flex-start;
+      flex-wrap: wrap;
+    }
+  }
+`
